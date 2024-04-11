b76165ec9a44a7fd1af39178a724733fe10f453d
Ignacy Nowak <inowak3@edu.cdv.pl>
2024-04-11 16:34:35 +0200

    projekt1

---
+++ Flappy Bird/Assets/BirdController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+    public float JumpForce;
+    public Rigidbody2D rb2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb2D.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);   
+        }
+    }
+}
